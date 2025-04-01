26d7bcb148c9b10f97ec8d21f5f7c0895febdd41
IsabelyFaria <bellyfaria04@gmail.com>
2025-04-01 11:37:22 -0300

    Feito codigo exercicio 1

---
+++ exercicio1.php
@@ -0,0 +1,18 @@
+# Faça um programa que exiba a mensagem “Bom dia, boa tarde, boa noite” 5 vezes.
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    for ($i = 0; $i < 5; $i++)
+    {
+        echo "Bom dia, boa tarde, boa noite <br>";
+    }
+    ?>
+</body>
+</html>
