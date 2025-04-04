1043b5b9bf7bcdebf1d694bbbafc04fd4634e67a
Miguel-Henrique-Felicio-Mussolini <mimi.hfm18@gmail.com>
2025-04-04 14:50:50 -0300

    Editado por causa de um erro do Github

--- exercicio6.php
+++ exercicio6.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>exercicio 6</title>
+    <title>Exercicio 6</title>
 </head>
 <body>
     <?php
