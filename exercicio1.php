d5700e4f78a75fde1966559b7259584efc951d51
Caua-Alborghette <cauaalborghette09@gmail.com>
2025-04-04 08:11:13 -0300

    Corrigindo erros

--- exercicio1.php
+++ exercicio1.php
@@ -1,11 +1,11 @@
 # Faça um programa que exiba a mensagem “Bom dia, boa tarde, boa noite” 5 vezes.
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>exercicio 1</title>
 </head>
 <body>
     <?php
---
+++ exercicio2.php
@@ -0,0 +1,19 @@
+#F.U.P. que exiba os números de 0 a 10 com intervalos de 1 em 1.
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercicio 1</title>
+</head>
+<body>
+    <?php
+    for ($i = 0; $i < 5; $i++)
+    {
+        echo "Bom dia, boa tarde, boa noite <br>";
+    }
+    ?>
+</body>
+</html>
