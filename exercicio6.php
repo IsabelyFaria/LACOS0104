dad9ca16eba3cfaa92bc553b048571afd5e43e82
Miguel-Henrique-Felicio-Mussolini <mimi.hf18@gmail.com>
2025-04-04 08:38:00 -0300

    Feito o exercicio 6

---
+++ exercicio6.php
@@ -0,0 +1,20 @@
+# F.U.P. que receba um número e mostre todos os números pares que existem de 0 até aquele número.
+
+<!DOCTYPE html>
+<html lang="pt br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercicio 6</title>
+</head>
+<body>
+    <?php
+    $numero = isset($_GET['numero']) ? (int)$_GET['numero'] : 0;
+    for( $i = 0; $i <= $numero; $i++) {
+        if ($i % 2 == 0) {
+            echo $i . "<br>";
+        }
+    }
+    ?>    
+</body>
+</html>
