2ed976deea900303457f6bf9e00d40803fb97aa5
IsabelyFaria <bellyfaria04@gmail.com>
2025-04-04 08:30:57 -0300

    correção de erros

--- exercicio4.php
+++ exercicio4.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>exercicio 2</title>
+    <title>exercicio 24</title>
 </head>
 <body>
     <?php
