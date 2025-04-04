6e068673df85e2d2d21ab85bd388ff16e188d351
IsabelyFaria <bellyfaria04@gmail.com>
2025-04-04 08:26:03 -0300

    correção de erros

---
+++ exercicio4.php
@@ -0,0 +1,21 @@
+#F.U.P. que receba uma palavra ou frase e escreva ela 10 vezes
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercicio 2/title>
+</head>
+<body>
+    <?php
+    if (isset($_GET['texto'])) {
+        $texto = htmlspecialchars($_GET['texto']);
+        for ($i = 0; $i < 10; $i++) {
+            echo $texto  . "<br>";
+        }
+    }
+
+    ?>
+</body>
+</html>
