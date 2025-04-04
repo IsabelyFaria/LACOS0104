4778050d048d27b9ab81ed6fa5f4d9400c298524
IsabelyFaria <duda.mefalmeida@gmail.com>
2025-04-04 08:21:12 -0300

    Feito codigo exercicio 3

---
+++ exercicio3.php
@@ -0,0 +1,19 @@
+#F.U.P. que exiba os números de 0 a 10 com intervalos de 3 em 3.
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercicio 2/title>
+</head>
+<body>
+    <?php
+     for ($i = 0; $i <= 10; $i += 3)
+    {
+    echo $i . "<br>";
+    }
+
+    ?>
+</body>
+</html>
