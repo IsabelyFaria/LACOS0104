51e86ad7f64faa2f35e8bc9203f47e28bd257ca0
IsabelyFaria <bellyfaria04@gmail.com>
2025-04-04 08:32:17 -0300

    feito o codigo exercicio 5

---
+++ exercicio5.php
@@ -0,0 +1,19 @@
+#F.U.P. que receba uma palavra e a quantidade de vezes que quer que ela seja repetida.
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercicio 5</title>
+</head>
+<body>
+    <?php
+    function repetirPalavra($palavra, $quantidade) {
+        for ($i = 0; $i < $quantidade; $i++) {
+            echo $palavra . "<br>";
+        }
+    }
+    ?>
+</body>
+</html>
